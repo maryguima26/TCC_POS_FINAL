874d5fe0b9c7f9154b9616c2cc9f976f7b7f2241
muriloarguello <muriloarguello@hotmail.com>
2022-12-18 12:01:19 -0300

    correcao dos hosts para deploy3

--- frontend/tfall/src/pages/professor/APIService.js
+++ frontend/tfall/src/pages/professor/APIService.js
@@ -1,19 +1,22 @@
 export default class APIService {
   static async UpdateDica(dica_id, body, token) {
-    const resp = await fetch(`http://15.228.156.155:8000/dicas/${dica_id}/`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Token ${token}`,
-        // Authorization: `Token 3efdb00ecf3504c8e839a3b97bfb083f2556599c`,
-      },
-      body: JSON.stringify(body),
-    });
+    const resp = await fetch(
+      `http://15.228.156.155:8000/api/dicas/${dica_id}/`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token}`,
+          // Authorization: `Token 3efdb00ecf3504c8e839a3b97bfb083f2556599c`,
+        },
+        body: JSON.stringify(body),
+      }
+    );
     return await resp.json();
   }
 
   static async InsertDica(body, token) {
-    const resp = await fetch("http://15.228.156.155:8000/dicas/", {
+    const resp = await fetch("http://15.228.156.155:8000/api/dicas/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -26,7 +29,7 @@ export default class APIService {
   }
 
   static DeleteDica(dica_id, token) {
-    return fetch(`http://15.228.156.155:8000/dicas/${dica_id}/`, {
+    return fetch(`http://15.228.156.155:8000/api/dicas/${dica_id}/`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
@@ -52,7 +55,7 @@ export default class APIService {
   }
 
   static async RegisterUser(body) {
-    const resp = await fetch("http://15.228.156.155:8000/user/", {
+    const resp = await fetch("http://15.228.156.155:8000/api/user/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
